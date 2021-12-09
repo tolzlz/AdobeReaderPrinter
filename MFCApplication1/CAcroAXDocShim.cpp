b55ae04dd0da985b097e85d7b977e99a2c077ea0
zhaoshoutao <tolzlz0636@gmail.com>
2021-12-09 13:53:12 +0800

    first commit

---
+++ MFCApplication1/CAcroAXDocShim.cpp
@@ -0,0 +1,14 @@
+// CAcroAXDocShim.cpp  : Definition of ActiveX Control wrapper class(es) created by Microsoft Visual C++
+
+
+#include "stdafx.h"
+#include "CAcroAXDocShim.h"
+
+/////////////////////////////////////////////////////////////////////////////
+// CAcroAXDocShim
+
+IMPLEMENT_DYNCREATE(CAcroAXDocShim, CWnd)
+
+// CAcroAXDocShim properties
+
+// CAcroAXDocShim operations
